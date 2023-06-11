fb1dedd2c584fc9217d5ebae7eb24aeec7fb96bb
akhoi <akhoitn@gmail.com>
2023-06-11 17:59:07 +0700

    Change some texts

--- Learn to C/Marco/main.c
+++ Learn to C/Marco/main.c
@@ -3,6 +3,13 @@
 
 #define BIG 15
 #define SUM(a,b) a+b
+#define CREATE_FUNC(name_func, cmd)\
+void name_func()                    \
+{                                   \
+    printf("%s\n", (char *)cmd);       \
+}
+
+CREATE_FUNC(say,"Try ur best");
 
 void display(){
     printf("Hi everyone\n");
@@ -10,7 +17,7 @@ void display(){
 
 int main(int argc, char const *argv[])
 {
-
+    say();
     display();
     printf("BIG: %d\n", BIG);
     printf("Sum a & b: %d\n", SUM(5,5));

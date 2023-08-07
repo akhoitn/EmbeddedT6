3ed1022d049658e7a2d604453ab39d5f4fb7212f
akhoi <akhoitn@gmail.com>
2023-08-07 18:50:39 +0700

    Change some texts

---
+++ Learn to C++/Store Management/Source/Table.cpp
@@ -0,0 +1,22 @@
+#include "../Header/Table.hpp"
+
+void Table::setStatus(st status)
+{
+    STATUS = status;
+}
+
+st Table::getStatus()
+{
+    return STATUS;
+}
+
+void addFoodToTable(Choose food)
+{
+    order.push_back(food);
+    STATUS == Close;
+}
+
+void backFood()
+
+
+void bill();
